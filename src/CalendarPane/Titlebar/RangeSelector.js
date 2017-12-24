00936b1e4a3cd3d511d94e4ea928d49f11a71ade
Alexander Zhang <alzhang96@gmail.com>
2017-12-23 21:51:45 -0500

    common button

--- src/CalendarPane/Titlebar/RangeSelector.js
+++ src/CalendarPane/Titlebar/RangeSelector.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './RangeSelector.css'
+import './RangeSelector.css';
+import './Button.css';
 
 class RangeSelector extends Component {
   render() {
     return (
       <div class="RangeSelector">
         <button class="ArrowButton"> &lt; </button>
-        <button class="TodayButton"> Today </button>
+        <button class="TextButton"> Today </button>
         <button class="ArrowButton"> &gt; </button>
       </div>
     );
